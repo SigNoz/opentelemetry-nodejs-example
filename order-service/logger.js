0575299df0e859736f4959ba9d79b5e55e578532
Yuvraj <yuvraajsj18@gmail.com>
2024-05-28 14:39:00 +0530

    trying to correlate traces and logs

--- order-service/logger.js
+++ order-service/logger.js
@@ -1,9 +1,25 @@
 // logger.js
 import pino from "pino";
+import { trace } from "@opentelemetry/api";
 
 const logger = pino({
   transport: {
     target: "pino-pretty",
+  },
+  formatters: {
+    log: (log) => {
+      const currentSpan = trace.getActiveSpan();
+      if (currentSpan) {
+        const { traceId, spanId } = currentSpan.spanContext();
+
+        log.traceId = traceId;
+        log.spanId = spanId;
+
+        console.log("Hereeee", traceId, " + ", spanId);
+      }
+      console.log(log);
+      return log;
+    },
   },
 });
 
